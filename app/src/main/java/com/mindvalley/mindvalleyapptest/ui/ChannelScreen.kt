c88b590e960095d3e5a46c8d0f894796ec70d5dc
Firdous <shahanifirdous@gmail.com>
2023-01-28 23:20:14 +0530

    - minor changes

--- app/src/main/java/com/mindvalley/mindvalleyapptest/ui/ChannelScreen.kt
+++ app/src/main/java/com/mindvalley/mindvalleyapptest/ui/ChannelScreen.kt
@@ -4,24 +4,45 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.mindvalley.mindvalleyapptest.R
+import com.mindvalley.mindvalleyapptest.domain.Resource
+import com.mindvalley.mindvalleyapptest.domain.model.MediaEntity
 import com.mindvalley.mindvalleyapptest.ui.theme.Typography
+import timber.log.Timber
 
 @Composable
-fun ChannelScreen(modifier: Modifier = Modifier){
-    Column(  modifier = modifier.padding(start = 20.dp, top = 50.dp, end = 20.dp, bottom = 20.dp)) {
+fun ChannelScreen(modifier: Modifier = Modifier, viewModel: ChannelViewModel){
+    Column( modifier = modifier.padding( top = 50.dp, bottom = 20.dp)) {
       Text(
+          modifier = modifier.padding(start = 20.dp, end = 20.dp),
           text = stringResource(id = R.string.label_channel),
           style = Typography.h4
       )
         Text(
-            modifier = modifier.padding(top = 30.dp),
+            modifier = modifier.padding(top = 30.dp, start = 20.dp, end = 20.dp),
             text = stringResource(id = R.string.label_new_episodes),
             style = Typography.h6,
         )
-        NewEpisodes()
+        var mediaEntity : List<MediaEntity>? = ArrayList()
+        val episodes by viewModel.episodeStateFlow.collectAsState(initial = Resource.Loading)
+        when(episodes){
+            is Resource.Loading -> {
+                Timber.e("Loading episodes")
+            }
+            is Resource.Success -> {
+                mediaEntity = (episodes as Resource.Success<List<MediaEntity>>).data
+            }
+            is Resource.Error -> {
+                Timber.e((episodes as Resource.Error).message)
+            }
+        }
+        if (mediaEntity != null) {
+            NewEpisodes(episodes = mediaEntity)
+        }
     }
 }
